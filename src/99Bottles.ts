e2e137ff2d3cc8f38008b5d48b48cab99246d34b
Colum Kelly <columk1@users.noreply.github.com>
2024-05-11 01:01:16 -0700

    Add exercise, 99 bottles

---
+++ src/99Bottles.ts
@@ -0,0 +1,22 @@
+export default class Bottles {
+  private numberOfBottles: number = 99
+  public verse = (n: number): string =>
+    n === 0
+      ? `No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n`
+      : n === 1
+      ? `${n} bottle of beer on the wall, ${n} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n`
+      : `${n} bottles of beer on the wall, ${n} bottles of beer.\nTake one down and pass it around, ${
+          n - 1
+        } bottle${n > 2 ? 's' : ''} of beer on the wall.\n`
+
+  public verses = (start: number, end: number): string => {
+    let result = ''
+    for (let i = start; i >= end; i--) {
+      result += this.verse(i)
+      if (i !== end) result += '\n'
+    }
+    return result
+  }
+
+  public song = (): string => this.verses(99, 0)
+}
